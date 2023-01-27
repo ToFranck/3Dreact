50b2c373da86e1609127f0a27d2ba431ca01a487
Franck Tolosano <tolosanofranck@hotmail.com>
2023-01-27 15:47:13 +0100

    super ouais

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Navbar.css"   
+
+export default function Navbar() {
+  return (
+    <div className='nav'>
+        <ul>
+
+            <li>Albums</li>
+            <li>About</li>
+        </ul> 
+    </div>
+  )
+}
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import backgroundvideo from '../../assets/backgroundvideo.mp4'
 import "./Home.css";
+// import Navbar from "../../components/Navbar/Navbar";
 
 import { Canvas } from "@react-three/fiber";
 import { useGLTF, Stage, PresentationControls } from "@react-three/drei";
@@ -16,21 +17,38 @@ export default function Home() {
 
   return (
     <div>
+      {/* <Navbar /> */}
       <div className="header">
         <video autoPlay loop muted>
                 <source src={backgroundvideo} type='video/mp4' />
-            </video>
-      </div>
+        </video>
 
+        <div className="container">
+          <div className="left">
 
-      <Canvas dpr={[1,2]} shadows camera={{fov: 45 }} style={{"position":"absolute"}}>
+          </div>
+          <div className="right">
+          
+          </div> 
+        </div>
+        <Canvas dpr={[1,2]} shadows camera={{fov: 45 }} style={{"position":"absolute",}}>
             {/* <color attach="background" args={['#101010']} /> */}
-            <PresentationControls speed={1.5} global zoom={.5} polar={[-0.1, Math.PI / 4 ]}>
+            <PresentationControls speed={1.5} 
+              // global zoom={1} 
+              polar={[-0.1, Math.PI / 4 ]}>
               <Stage environment={null}>
                 <Model scale={0.01} />
               </Stage>
             </PresentationControls>
-      </Canvas>
+        </Canvas>
+      
+      </div>
+
+    
+
+      
+
+    
     </div>
   );
 }
