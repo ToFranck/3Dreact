b446846cd713a9cd68d986e59f844a80c68119be
ToFranck <73122097+ToFranck@users.noreply.github.com>
2023-01-29 23:17:50 +0100

    maj

---
+++ src/components/Model.jsx
@@ -0,0 +1,24 @@
+import React, { useRef } from 'react'
+import { useGLTF } from '@react-three/drei'
+import { useFrame } from '@react-three/fiber'
+
+export default function Model(props) {
+
+
+        const { scene } = useGLTF('/cd.glb');
+        const ref = useRef  (null);
+        useFrame (() => {
+            if (!ref.current) {
+                return;
+            }
+            ref.current.rotation.x += 0.01;
+            // ref.current.rotation.y += 0.01;
+            ref.current.rotation.z += 0.01;
+            
+
+        });
+
+        return <primitive object={scene} scale={0.01} {...props} ref={ref}  />;
+   
+      
+}
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,54 +1,48 @@
 import React from "react";
-import backgroundvideo from '../../assets/backgroundvideo.mp4'
+import backgroundvideo from "../../assets/backgroundvideo.mp4";
 import "./Home.css";
-// import Navbar from "../../components/Navbar/Navbar";
 
 import { Canvas } from "@react-three/fiber";
-import { useGLTF, Stage, PresentationControls } from "@react-three/drei";
-
-
-function Model(props) {
-  const { scene } = useGLTF('/cd.glb');
-  return <primitive object={scene} scale={0.01} {...props} />;
-}
+import { Stage, PresentationControls } from "@react-three/drei";
+import Model from "../../components/Model";
 
 export default function Home() {
- 
-
   return (
     <div>
       {/* <Navbar /> */}
-      <div className="header">
+      <div className="main">
         <video autoPlay loop muted>
-                <source src={backgroundvideo} type='video/mp4' />
+          <source src={backgroundvideo} type="video/mp4" />
         </video>
 
-        <div className="container">
-          <div className="left">
-
-          </div>
-          <div className="right">
-          
-          </div> 
+        <div className="head">
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse nesciunt ab quibusdam deserunt molestias, labore laboriosam odio inventore consequuntur a eligendi non error repellendus minus est voluptatibus delectus ratione qui?</p>
         </div>
-        <Canvas dpr={[1,2]} shadows camera={{fov: 45 }} style={{"position":"absolute",}}>
-            {/* <color attach="background" args={['#101010']} /> */}
-            <PresentationControls speed={1.5} 
-              // global zoom={1} 
-              polar={[-0.1, Math.PI / 4 ]}>
-              <Stage environment={null}>
-                <Model scale={0.01} />
-              </Stage>
-            </PresentationControls>
-        </Canvas>
-      
+
+
+        <div className="content">
+          <div className="left">left</div>
+          <div className="right">
+            right
+            <Canvas
+              dpr={[1, 2]}
+              camera={{ fov: 45 }}
+              style={{ width: "300px", height: "500px" }}
+            >
+              {/* <color attach="background" args={['#101010']} /> */}
+              <PresentationControls
+                speed={2}
+                // global zoom={1}
+                polar={[-0.1, Math.PI / 4]}
+              >
+                <Stage environment={null}>
+                  <Model scale={0.01} />
+                </Stage>
+              </PresentationControls>
+            </Canvas>
+          </div>
+        </div>
       </div>
-
-    
-
-      
-
-    
     </div>
   );
 }
